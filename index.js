ddc4f9b6441f71c355fc682bb1915407d6ea0bbc
Rodney Abad <r.abadarocha@gmail.com>
2024-02-16 17:19:13 -0600

    new

---
+++ index.js
@@ -0,0 +1,45 @@
+//modules
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+//app resources
+const routes = require('./app/routes/routes');
+const { url } = require('./app/config/dataBase');
+
+
+
+//DB Connecion
+mongoose.set('strictQuery', true);
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+	if (err) {
+		console.log(err);
+	}
+	console.log('conectado a MongoDB');
+});
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname, '/public')));
+
+//settings
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3999);
+
+//middlewares
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 100000, limit: "500mb" }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(routes);
+
+//starting server
+const server = app.listen(app.get('port'), () => {
+	console.log('Server en ' + app.get('port'));
+});
